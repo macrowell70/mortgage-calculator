9cd0ce0a444017d0d050d9bac05645593272c595
unknown <macrowell@yahoo.com>
2022-10-07 09:07:38 -0400

    uses map function to clean up code in mortgage calculator page

--- src/Components/MortgageCalculator.js
+++ src/Components/MortgageCalculator.js
@@ -3,23 +3,30 @@ import React, { useState, useEffect } from 'react';
 function MortgageCalculator(props) {
     const { income, expenses, dti, maxPayment, maxMortgage } = props.finalFigures;
 
+    const fields = [{
+        figure: income,
+        lable: 'Monthly income'
+    }, {
+        figure: expenses,
+        lable: 'Monthly expenses'
+    }, {
+        figure: dti,
+        lable: 'Debt-to-income ratio'
+    }, {
+        figure: maxPayment,
+        lable: 'Maximum monthly payment'
+    }, {
+        figure: maxMortgage,
+        lable: 'Maximum mortgage principle'
+    }];
+
     return (
         <div>
-            <div id='mortgage-income' className='mortgage-field' >
-               {`Monthly income $${income}`} 
-            </div>
-            <div id='mortgage-expenses' className='mortgage-field' >
-                {`Monthly expenses $${expenses}`}
-            </div>
-            <div id='mortgage-dti' className='mortgage-field' >
-                {`Debt-to-income ratio ${dti}%`}
-            </div>
-            <div id='mortgage-payment' className='mortgage-field' >
-                {`Maximum monthly payment $${maxPayment}`}
-            </div>
-            <div id='mortgage-principle' className='mortgage-field' >
-                {`Maximum mortgage principle $${maxMortgage}`}
-            </div>
+            {fields.map((field, i) => (
+                <div className='mortgage-field' key={i} >
+                    {`${field['lable']} ${field['figure']}`}
+                </div>
+            ))}
         </div>
     );
 };
